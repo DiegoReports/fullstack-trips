70dfad1709201833c483e8dc4eed4b61d82e99a6
Diego Henrique <diego.reports@gmail.com>
2023-07-06 12:26:59 -0300

    feat: add Trips component

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,7 +1,28 @@
+import { prisma } from '@/lib/prisma'
 import React from 'react'
 
-const Trips = () => {
-  return <div>Trips</div>
+const getTrips = async () => {
+  const trips = await prisma.trip.findMany({})
+
+  return trips
+}
+
+const Trips = async () => {
+  //const data = await getTrips()
+  const data = await fetch('http://jsonplaceholder.typicode.com/posts').then(
+    res => res.json()
+  )
+
+  console.log({ data })
+
+  //return <div>Trips</div>
+  return (
+    <div>
+      {data.map(i => (
+        <p key={i.id}>{i.title}</p>
+      ))}
+    </div>
+  )
 }
 
 export default Trips
